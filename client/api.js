459d49e361d9f8f82e3db8a8a8165dd8cbc4e4ff
shaun-collinss <shaun_collins@hotmail.co.nz>
2022-10-28 18:07:55 +1300

    stuff

--- client/api.js
+++ client/api.js
@@ -2,6 +2,8 @@ import request from 'superagent'
 
 const serverURL = 'http://localhost:3000/api/v1'
 
+export function 
+
 // *** EXAMPLE ***
 export function getWelcome () {
   return request
@@ -9,3 +11,10 @@ export function getWelcome () {
     .then(response => response.body)
 }
 // ***   ***   ***
+
+
+export function getTeams () {
+  return request
+    .get(`${serverURL}/teams`)
+    .then(response => response.body)
+}
